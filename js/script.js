412532620f804cc06095a49228d5fa4ee5295c3f
androfficial <material1fox@gmail.com>
2021-08-03 00:10:50 +0300

    1.0

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let isMobile = {
+   Android: function() {return navigator.userAgent.match(/Android/i);},
+   BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+   iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+   Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+   Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+   any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
+document.addEventListener('click', (e) => {
+   if (!e.target.closest('.info-header__column')) {
+      for (let infoHeaderColumn of document.querySelectorAll('.info-header__column')) {
+         infoHeaderColumn.classList.remove('_show');
+      }
+   }
+});
+const infoHeader = document.querySelector('.info-header');
+
+if (infoHeader) {
+   infoHeader.addEventListener('click', (e) => {
+      if (e.target.classList.contains('info-header__current-lang') || e.target.classList.contains('_icon')) {
+         e.target.parentElement.classList.toggle('_show');
+      }
+   });
+}
