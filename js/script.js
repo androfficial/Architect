b9b2dc13cb84e21519dd49e7f1c6a386ae6d0f36
androfficial <material1fox@gmail.com>
2021-08-03 11:04:51 +0300

    1.01

--- js/script.js
+++ js/script.js
@@ -18,7 +18,30 @@ const infoHeader = document.querySelector('.info-header');
 if (infoHeader) {
    infoHeader.addEventListener('click', (e) => {
       if (e.target.classList.contains('info-header__current-lang') || e.target.classList.contains('_icon')) {
-         e.target.parentElement.classList.toggle('_show');
+         let parent = e.target.parentElement;
+         if (parent.classList.contains('info-header__column--region')) {
+            if (document.querySelector('.info-header__column--lang').classList.contains('_show')) {
+               document.querySelector('.info-header__column--lang._show').classList.remove('_show')
+            }
+            parent.classList.toggle('_show');
+         } else {
+            if (document.querySelector('.info-header__column--region').classList.contains('_show')) {
+               document.querySelector('.info-header__column--region._show').classList.remove('_show')
+            }
+            parent.classList.toggle('_show');
+         }
+      }
+      if (e.target.closest('.info-header__submenu') && e.target.classList.contains('info-header__link')) {
+         e.preventDefault();
+         let openMenu = document.querySelector('.info-header__column._show');
+         let openMenuFirstElem = openMenu.firstElementChild;
+         let firstElemContent = openMenuFirstElem.innerHTML;
+         let newContent = e.target.innerHTML;
+
+         openMenuFirstElem.innerHTML = newContent;
+         e.target.innerHTML = firstElemContent;
+
+         openMenu.classList.remove('_show');
       }
    });
 }
