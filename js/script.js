03c26a0f77f60669a6800db9f0d8c7c8bef64f68
androfficial <material1fox@gmail.com>
2021-08-05 12:17:21 +0300

    1.09

--- js/script.js
+++ js/script.js
@@ -59,3 +59,43 @@ new Swiper('.holiday__items', {
 	},
 
 });
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+   
+   let error = checkValueInput(form);
+   if (error === 0) {
+      console.log('Все поля успешно заполнены.');
+   } else {
+      console.log('Пожалуйста, заполните поля.');
+   }
+});
+
+const checkValueInput = (form) => { 
+   let error = 0;
+   const inputs = form.querySelectorAll('._req');
+
+   for (let input of inputs) {
+      input.classList.remove('_error', '_fld-email');
+      if (input.classList.contains('_email')) {
+         if (emailTest(input)) {
+            input.classList.add('_error', '_fld-email');
+            error++;
+         }
+      } else {
+         if (input.value === '') {
+            input.classList.add('_error');
+            error++;
+         }
+      }
+
+   }
+
+   return error;
+   
+};
+
+function emailTest(input) {
+   return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
